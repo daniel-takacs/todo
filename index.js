c8f7c93184ca9ceb8c7820006ee16deecffcbf79
dta <daniel.takacs@sofiadigital.com>
2024-02-12 12:12:08 +0200

    Add Todo model, update routes, and configure MongoDB connection

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+import express from 'express';
+import mongoose from 'mongoose';
+import todoRoutes from './routes/todo.routes.js';
+import cors from 'cors'
+import * as dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000; 
+app.use(cors({
+  origin: 'https://fullstack-todo-vue.netlify.app' 
+}));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json());
+const mongoUri = process.env.DB_URI;
+// MongoDB Connection
+// const mongoUri = 'mongodb://localhost:27017/todoApp';
+
+if (!mongoUri) {
+  console.error('MongoDB URL is not defined in the environment variables.');
+} else {
+  mongoose
+    .connect(mongoUri, {
+    })
+    .then(() => {
+      console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+      console.error('Error connecting to MongoDB:', err);
+    });
+}
+
+
+app.use('/api', todoRoutes);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Server is running on port ${port}`);
+});
